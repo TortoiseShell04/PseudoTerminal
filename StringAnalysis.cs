a0aab9a6ab8fc0b9cb47c80995a5f6d27f6e3279
TortoiseShell04 <cutiecatdaog@gmail.com>
2022-12-05 20:50:31 +0200

    Added new fixes

--- StringAnalysis.cs
+++ StringAnalysis.cs
@@ -21,6 +21,11 @@ namespace PseudoTerminal
                     Console.WriteLine($"/>>> {text[i]} is a vowel"); 
                 }
 
+                else if (text[i] == '0' || text[i] == '1' || text[i] == '2' || text[i] == '3' || text[i] == '4' ||
+                         text[i] == '5' || text[i] == '6' || text[i] == '7' || text[i] == '8' || text[i] == '9' )
+                {
+                    Console.WriteLine($"/>>> {text[i]} is a number");
+                }
                 else if (text[i] == ' ')
                 {
                     // DO NOTHING
--- Terminal.cs
+++ Terminal.cs
@@ -59,6 +59,7 @@ namespace PseudoTerminal
                 {
                     low.Lower();
                 }
+                
             }
         }
     }
